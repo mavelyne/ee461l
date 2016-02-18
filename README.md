bf1cb30648e700849ebb4a3fa1f0f393257b7c89
Anoop Naravaram <anoop901@gmail.com>
2016-02-17 19:04:06 -0600

    deployed to Heroku

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ee461l
+
+Available at: http://trii-chat.herokuapp.com
+
+How to deploy a SparkJava application to Heroku:
+https://sparktutorials.github.io/2015/08/24/spark-heroku.html
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Spark.port(Main.getHerokuAssignedPort());
+
         // respond to root request
         Spark.get("/", ((request, response) -> "Hello, EE 461L!"));
+    }
+
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
